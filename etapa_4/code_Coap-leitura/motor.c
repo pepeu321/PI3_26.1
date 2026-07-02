a26d8b5f7b134944353cb2605633585c6f0b74b6
AndreOuti <107962002+AndreOuti@users.noreply.github.com>
2026-07-02 18:31:08 -0300

    Add files via upload

---
+++ etapa_4/code_Coap-leitura/motor.c
@@ -0,0 +1,10 @@
+/*
+ * motor.c
+ *
+ *  Created on: 2 de jul. de 2026
+ *      Author: decoo
+ */
+
+
+
+
---
+++ etapa_4/code_Coap-leitura/motor.h
@@ -0,0 +1,15 @@
+/*
+ * motor.h
+ *
+ *  Created on: 2 de jul. de 2026
+ *      Author: decoo
+ */
+
+#ifndef MAIN_MOTOR_H_
+#define MAIN_MOTOR_H_
+
+
+
+
+
+#endif /* MAIN_MOTOR_H_ */
